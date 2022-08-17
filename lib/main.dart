efd86518096c505fce7d356bc9468ae81608e600
boni <kgb8594@gmail.com>
2022-08-17 15:13:58 +0900

    Chapter 59. StateNotifier & StateNotifierProvider example 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:state_notifier_ex/providers/bg_color.dart';
+import 'package:state_notifier_ex/providers/counter.dart';
+import 'package:state_notifier_ex/providers/customer_level.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,58 +14,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MultiProvider(
+      providers: [
+        StateNotifierProvider<BgColor, BgColorState>(create: (_) => BgColor()),
+        StateNotifierProvider<Counter, CounterState>(create: (_) => Counter()),
+        StateNotifierProvider<CustomerLevel, Level>(
+            create: (_) => CustomerLevel()),
+      ],
+      builder: (context, _) {
+        return MaterialApp(
+          title: 'StateNotifier',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(),
+        );
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final colorState = context.watch<BgColorState>();
+    final counterState = context.watch<CounterState>();
+    final levelState = context.watch<Level>();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('StateNotifier'),
+        backgroundColor: colorState.color,
       ),
+      backgroundColor: levelState == Level.bronze
+          ? Colors.white
+          : levelState == Level.silver
+              ? Colors.grey
+              : Colors.yellow,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Text(
+          '${counterState.counter}',
+          style: Theme.of(context).textTheme.headline2,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<Counter>().increment(),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () => context.read<BgColor>().changeColor(),
+            tooltip: 'Change Color',
+            child: const Icon(Icons.color_lens_outlined),
+          ),
+        ],
       ),
     );
   }
